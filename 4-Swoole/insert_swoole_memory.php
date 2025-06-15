74a4fd909e370f7623b202b35128cc146cf7da2a
Leonardo Tumadjian <tumadjian@gmail.com>
2025-06-14 21:56:34 -0300

    Add slides

---
+++ 4-Swoole/insert_swoole_memory.php
@@ -0,0 +1,40 @@
+<?php
+
+ini_set('memory_limit', '512M');
+
+use function Swoole\Coroutine\go;
+use function Swoole\Coroutine\run;
+
+require __DIR__ . '/vendor/autoload.php';
+
+run(function () {
+    $pdoConfig = new \Swoole\Database\PDOConfig()
+        ->withHost('mariadb')
+        ->withDbname('bench')
+        ->withUsername('root')
+        ->withPassword('123456')
+    ;
+
+    $pool = new \Swoole\Database\PDOPool($pdoConfig, 50);
+
+    $pdo = $pool->get();
+    // Limpa a tabela
+    $pdo->exec("TRUNCATE TABLE users");
+    $pool->put($pdo);
+
+    for ($i = 0; $i < 30000; $i++) {
+        $faker = new Faker\Factory()->create();
+        $name = $faker->name();
+        $email = $faker->email();
+        go(function () use ($pool, $name, $email) {
+            $pdo = $pool->get();
+
+            $stmt = $pdo->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
+            $stmt->execute([$name, $email]);
+
+            $pool->put($pdo);
+        });
+    }
+});
+
+echo 'Total de memoria usada: ' . (round(memory_get_peak_usage() / 1024 / 1024, 2)) . 'MB' . PHP_EOL;
--- 4-Swoole/server.php
+++ 4-Swoole/server.php
@@ -1,4 +1,30 @@
 <?php
 
+
+use Swoole\Constant;
+
 require __DIR__ . '/vendor/autoload.php';
+
+\Swoole\Runtime::enableCoroutine();
+
+$server = new Swoole\Http\Server('0.0.0.0', 8080, SWOOLE_BASE);
+
+$server->set([
+    'worker_num' => 4,
+]);
+
+$server->on(Constant::EVENT_START, function ($server) {
+    echo "Swoole http server is started at http://0.0.0.0:8080" . PHP_EOL;
+});
+
+$server->on(Constant::EVENT_REQUEST, function ($request, $response) {
+    $pdo = new PDO('mysql:dbname=bench;host=mariadb', 'root', '123456');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->query('SELECT * FROM users WHERE id = 1 AND SLEEP(2) = 0');
+
+    $response->end(json_encode($stmt->fetchAll(PDO::FETCH_OBJ)));
+});
+
+$server->start();
 
